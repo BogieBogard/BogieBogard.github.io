a0af3648f47a74fa43d7489b351bd0d4aa218f04
BogieBogard <mbogard@utexas.edu>
2018-08-16 13:55:12 -0500

    contactform.php test1

---
+++ assets/php/contactform.php
@@ -0,0 +1,15 @@
+<?php
+
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $mailFrom = $_POST['email'];
+    $phone = $_POST['phone'];
+    $message = $_POST['message'];
+
+    $mailTo = "mbogard@utexas.edu";
+    $headers = "From: ".$mailFrom;
+    $txt = "You have received an email from ".$name.".\n\n".$message; 
+
+    mail($mailTo, $phone, $txt, $headers);
+    header("Location: index.php?mailsend");
+}
