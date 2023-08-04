aa4d7caa36cdffb6ddd00d921e39a25467481a3b
BogieBogard <mbogard@utexas.edu>
2023-08-04 10:06:03 -0500

    increase typing speed

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -9,6 +9,37 @@ const TxtType = function(el, toRotate, period) {
 	this.isDeleting = false;
 };
 
+// TxtType.prototype.tick = function() {
+// 	var i = this.loopNum % this.toRotate.length;
+// 	var fullTxt = this.toRotate[i];
+
+// 	if (this.isDeleting) {
+// 	this.txt = fullTxt.substring(0, this.txt.length - 1);
+// 	} else {
+// 	this.txt = fullTxt.substring(0, this.txt.length + 1);
+// 	}
+
+// 	this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+// 	var that = this;
+// 	var delta = 200 - Math.random() * 100;
+
+// 	if (this.isDeleting) { delta /= 2; }
+
+// 	if (!this.isDeleting && this.txt === fullTxt) {
+// 	delta = this.period;
+// 	this.isDeleting = true;
+// 	} else if (this.isDeleting && this.txt === '') {
+// 	this.isDeleting = false;
+// 	this.loopNum++;
+// 	delta = 500;
+// 	}
+
+// 	setTimeout(function() {
+// 	that.tick();
+// 	}, delta);
+// };
+
 TxtType.prototype.tick = function() {
 	var i = this.loopNum % this.toRotate.length;
 	var fullTxt = this.toRotate[i];
@@ -22,7 +53,7 @@ TxtType.prototype.tick = function() {
 	this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
 
 	var that = this;
-	var delta = 200 - Math.random() * 100;
+	var delta = 100 - Math.random() * 50;
 
 	if (this.isDeleting) { delta /= 2; }
 
@@ -32,7 +63,7 @@ TxtType.prototype.tick = function() {
 	} else if (this.isDeleting && this.txt === '') {
 	this.isDeleting = false;
 	this.loopNum++;
-	delta = 500;
+	delta = 250;
 	}
 
 	setTimeout(function() {
